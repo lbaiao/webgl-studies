3dae0e5fcbaea0955ad0288154b0a90eb6ee7f11
Lucas Pires <LucasP@leapthought.co.nz>
2022-03-18 00:55:25 -0300

    added new tutos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -8,6 +8,8 @@ import {
 } from "react-router-dom";
 import Tuto1 from './experiments/tuto1';
 import Tuto2 from './experiments/tuto2';
+import Tuto3 from './experiments/tuto3';
+import Tuto4 from './experiments/tuto4';
 
 function App() {
   return (
@@ -28,8 +30,18 @@ function App() {
         </header>*/}
       <Router>
         <Routes>
+          {/* draw simple triangles using webgl1.0 */}
           <Route path="/tuto1" element={<Tuto1></Tuto1>} />
+
+          {/* draw random triangles using webgl2.0 */}
           <Route path="/tuto2" element={<Tuto2></Tuto2>} />
+
+          {/* draw random rectangles using webgl2.0 */}
+          <Route path="/tuto3" element={<Tuto3></Tuto3>} />
+
+          {/* apply translation to a rectangle */}
+          <Route path="/tuto4" element={<Tuto4></Tuto4>} />
+
           <Route path="/users" element={<Users></Users>} />
           <Route path="/" element={<Home></Home>} />
         </Routes>
