7ed745870400af4a3d37d9763e0b9d05db08a3e6
Lucas Pires <LucasP@leapthought.co.nz>
2022-03-18 00:56:16 -0300

    reusable createShader and createProgram util functions for webgl

--- src/utils/webglUtils.tsx
+++ src/utils/webglUtils.tsx
@@ -17,3 +17,30 @@ export function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement, multiplier:
   }
   return false;
 }
+
+export function createShader(gl: WebGL2RenderingContext, type: number, source: string) {
+  var shader = gl.createShader(type) as WebGLShader;
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+export function createProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
+  var program = gl.createProgram() as WebGLProgram;
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
